f253a9bc332442d2d11e8b7c46d7d0aebcd8a2c7
bedvinas <edvix08@gmail.com>
2018-01-06 17:09:43 +0100

    Collision
    Collision

---
+++ Assets/Scripts/Attackers.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Attackers : MonoBehaviour {
+
+    [Range(-1f, 1.5f)]
+    public float walkSpeed;
+
+	// Use this for initialization
+	void Start () {
+        Rigidbody2D myRigidbody = gameObject.GetComponent<Rigidbody2D>();
+        myRigidbody.isKinematic = true;
+        
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        transform.Translate(Vector3.left * walkSpeed * Time.deltaTime);
+	}
+
+    void OnTriggerEnter2D() {
+        Debug.Log(name + " trigger entered");
+    }
+}
---
+++ Assets/Scripts/SetStartVolume.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SetStartVolume : MonoBehaviour {
+
+    private MusicManager musicManager;
+
+	// Use this for initialization
+	void Start () {
+        musicManager = GetComponent<MusicManager>();
+        if (musicManager)
+        {
+            float volume = PlayerPrefsManager.GetMasterVolume();
+            musicManager.ChangeVolume(volume);
+        }
+        else {
+            Debug.LogWarning("Music manager not found in start scene, can't set volume");
+        }
+        
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
